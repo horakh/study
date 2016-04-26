46e92baa4961a415ae28ff3b203cf5f019a492a2
Hryhoriy Horak <horakh@ukr.net>
2016-04-26 18:03:59 +0300

    exception

--- Library/Library/Program.cs
+++ Library/Library/Program.cs
@@ -3,13 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Library
 {
+    public enum BookType
+    {
+        SiFi, Fantasy
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var color =
+            Console.ForegroundColor = ConsoleColor.White;            
+            Console.WriteLine("Loading Library Information...");
+
+
+            // try catch finaly 
+            Stream stream;
+            StreamReader reader = null;
+            try
+            {
+                throw new FileNotFoundException("Dupa.txt");
+                stream = new FileStream("Content/library12.txt", FileMode.Open);
+                reader = new StreamReader(stream);
+                var text = reader.ReadToEnd();
+                Console.WriteLine(text);
+            }
+            catch (DivideByZeroException ex0)
+            {
+                Console.WriteLine("Divided by 0 Catched");
+                
+            }
+            catch (FileNotFoundException exfnf)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Could not load configuration file {exfnf.Message}");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            finally
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                if (reader!=null) reader.Dispose();
+            }
+
+            using (StreamReader r = new StreamReader("Content/library.txt"))
+            {
+                while (!r.EndOfStream)
+                {
+
+                    Console.WriteLine(r.ReadLine());
+                }
+            }
+
+                Console.WriteLine("Loaded");
+            Console.ReadKey();
         }
     }
 }
